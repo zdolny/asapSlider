0307ea4cd916b14b35e4ee35dae638ce77a4bc8e
Michał Dolny <michal.dolny@gmail.com>
2013-11-26 22:13:02 +0100

    switch handle from options param to this

--- elmo-slider.js
+++ elmo-slider.js
@@ -1,25 +1,20 @@
 //  elmoSlider v0.0.2 by Michal Dolny
 //  dependencies: jQuery
-//
-//  options: { optionName: value, optionName2: value2 }
-//  optionName:
-//      sliderContent - class/id of wrapper with all slides inside
 
-
-$.fn.elmoSlider = function(options) {
+$.fn.elmoSlider = function() {
     // 3 slides, each 1020px wide
     // state represents margin that will be applied to each slide
     var state = ['0', '-1020px', '-2040px'],
         i = 0,  // start point
-        slider = options.sliderContent;
+        slider = this;
     $('.slide-left').on('click', function(){
         i--;
         if( i < 0 ){ i = 2 };
-        $(slider).animate({'margin-left': state[i]}, 'slow');
+        slider.animate({'margin-left': state[i]}, 'slow');
     });
     $('.slide-right').on('click', function(){
         i++;
         if( i > 2 ){ i = 0 };
-        $(slider).animate({'margin-left': state[i]}, 'slow');
+        slider.animate({'margin-left': state[i]}, 'slow');
     });
 };
