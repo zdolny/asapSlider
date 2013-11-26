07ef8f97679a692c6101b7619d94091cc6774f3a
Michał Dolny <michal.dolny@gmail.com>
2013-11-26 21:54:30 +0100

    turn into jQuery plugin

--- elmo-slider.js
+++ elmo-slider.js
@@ -1,21 +1,25 @@
-/* elmoSlider v0.0.1 by Michal Dolny */
-/* dependencies: jQuery */
+//  elmoSlider v0.0.2 by Michal Dolny
+//  dependencies: jQuery
+//
+//  options: { optionName: value, optionName2: value2 }
+//  optionName:
+//      sliderContent - class/id of wrapper with all slides inside
 
-$(function(){
-    var elmoSlider = {
-        /* 3 slides, each 1020px wide */
-        /* state represents margin that will be applied to each slide */
-        state: ['0', '-1020px', '-2040px'],
-        i: 0
-    };
+
+$.fn.elmoSlider = function(options) {
+    // 3 slides, each 1020px wide
+    // state represents margin that will be applied to each slide
+    var state = ['0', '-1020px', '-2040px'],
+        i = 0,  // start point
+        slider = options.sliderContent;
     $('.slide-left').on('click', function(){
-        elmoSlider.i--;
-        if( elmoSlider.i < 0 ){ elmoSlider.i = 2 };
-        $('.slider-content').animate({'margin-left': elmoSlider.state[elmoSlider.i]}, 'slow');
+        i--;
+        if( i < 0 ){ i = 2 };
+        $(slider).animate({'margin-left': state[i]}, 'slow');
     });
     $('.slide-right').on('click', function(){
-        elmoSlider.i++;
-        if( elmoSlider.i > 2 ){ elmoSlider.i = 0 };
-        $('.slider-content').animate({'margin-left': elmoSlider.state[elmoSlider.i]}, 'slow');
+        i++;
+        if( i > 2 ){ i = 0 };
+        $(slider).animate({'margin-left': state[i]}, 'slow');
     });
-});
+};
