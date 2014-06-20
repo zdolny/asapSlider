188b68757ea7732a8e3e623893b73f2b727138db
Michał Dolny <michal.dolny@gmail.com>
2014-06-20 02:00:25 +0200

    configuration options (number of slides, slide width)

--- elmo-slider.js
+++ elmo-slider.js
@@ -1,12 +1,18 @@
 //  elmoSlider v0.0.2 by Michal Dolny
 //  dependencies: jQuery
 
-$.fn.elmoSlider = function() {
-    // 3 slides, each 360px wide
+$.fn.elmoSlider = function(n, x) {
+    // n - number of slides
+    // x - slide width
+    // example - 3 slides, each 360px wide
     // state represents margin that will be applied to each slide
-    var state = ['0', '-360px', '-720px'],
+    var state = [0],
         i = 0,  // start point
         slider = this;
+    for(var j=1; j<n; j++){
+        state[j]=-(j*x);
+    }
+    console.log(state);
     $('.slide-left').on('click', function(){
         i--;
         if( i < 0 ){ i = 2 };
