01820a57fc8504bf3e6522360679a9f3d73d2f39
Michał Dolny <michal.dolny@gmail.com>
2014-10-20 23:04:36 +0200

    wrap in each

--- jquery.elmoslider.js
+++ jquery.elmoslider.js
@@ -22,67 +22,69 @@
             j++;
         });
 
-        // setup css
-        if (settings.transition === 'slide'){
-            slider.find('.slider-content').css({
-                width: (state.length * 100 + '%')
-            });
-            slider.find('.slide').css({
-                width: (100 / state.length + '%'),
-                'float': 'left',
-            });
-        } else if (settings.transition === 'fade'){
-            slider.find('.slider-content').css({
-                width: (100 + '%')
-            });
-            slider.find('.slide').css({
-                width: (100 + '%'),
-                'float': 'none',
-                position: 'absolute',
-                opacity: 0
-            });
-        }
-        $(slider).find('.slide').first().addClass('active-slide').css('opacity', '1');
+        return this.each(function(){
 
-        if (settings.pause <= settings.speed){
-            settings.pause = settings.speed + 100;
-        }
+            // setup css
+            if (settings.transition === 'slide'){
+                slider.find('.slider-content').css({
+                    width: (state.length * 100 + '%')
+                });
+                slider.find('.slide').css({
+                    width: (100 / state.length + '%'),
+                    'float': 'left',
+                });
+            } else if (settings.transition === 'fade'){
+                slider.find('.slider-content').css({
+                    width: (100 + '%')
+                });
+                slider.find('.slide').css({
+                    width: (100 + '%'),
+                    'float': 'none',
+                    position: 'absolute',
+                    opacity: 0
+                });
+            }
+            $(slider).find('.slide').first().addClass('active-slide').css('opacity', '1');
 
-        if (settings.autoPlay === false){
-            $(slider).on('click', '.slide-left', function(){
-                i--;
-                if ( i < 0 ){ i = 2 };
+            if (settings.pause <= settings.speed){
+                settings.pause = settings.speed + 100;
+            }
+
+            if (settings.autoPlay === false){
+                $(slider).on('click', '.slide-left', function(){
+                    i--;
+                    if ( i < 0 ){ i = 2 };
+                    if (settings.transition === 'fade'){
+                        $('.active-slide').removeClass('active-slide').animate({'opacity': 0}, settings.speed);
+                        $('.slide'+(i+1)).addClass('active-slide').animate({'opacity': 1}, settings.speed);
+                    } else {
+                        $(slider).find('.slider-content').animate({'margin-left': state[i]}, settings.speed);
+                    };
+                });
+                $(slider).on('click', '.slide-right', function(){
+                    slideForward();
+                });
+            }
+
+            if (settings.autoPlay === true){
+                setInterval(function(){
+                    slideForward();
+                }, settings.pause);
+            }
+
+            function slideForward(){
+                i++;
+                if ( i > 2 ){ i = 0 };
                 if (settings.transition === 'fade'){
                     $('.active-slide').removeClass('active-slide').animate({'opacity': 0}, settings.speed);
                     $('.slide'+(i+1)).addClass('active-slide').animate({'opacity': 1}, settings.speed);
                 } else {
                     $(slider).find('.slider-content').animate({'margin-left': state[i]}, settings.speed);
                 };
-            });
-            $(slider).on('click', '.slide-right', function(){
-                slideForward();
-            });
-        }
+            }
 
-        if (settings.autoPlay === true){
-            setInterval(function(){
-                slideForward();
-            }, settings.pause);
-        }
+        });
 
-        function slideForward(){
-            i++;
-            if ( i > 2 ){ i = 0 };
-            if (settings.transition === 'fade'){
-                $('.active-slide').removeClass('active-slide').animate({'opacity': 0}, settings.speed);
-                $('.slide'+(i+1)).addClass('active-slide').animate({'opacity': 1}, settings.speed);
-            } else {
-                $(slider).find('.slider-content').animate({'margin-left': state[i]}, settings.speed);
-            };
-        }
-
-        // make plugin method chainable
-        return this;
     };
 
 })(jQuery);
